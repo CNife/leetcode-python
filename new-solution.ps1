57f591a428f32fff94803efe9a8df538cd803bf7
CNife <CNife@vip.qq.com>
2021-04-30 14:47:19 +0800

    更新创建新题解的脚本

--- new-solution.ps1
+++ new-solution.ps1
@@ -10,4 +10,6 @@ if ($Title -match '_(i{1,3})$')
 $Title = $Title -replace '_iv$', '_4'
 $Title = $Title -replace '_v$', '_5'
 
-Write-Host $Title
+$SolutionPath = "$PSScriptRoot\code\$Title.py"
+Write-Host "`n" > $SolutionPath
+pycharm $SolutionPath
